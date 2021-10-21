904c10c736d03f0aad301fe64442ea16683bb9f4
Akshaj (Personal) <akshaj@dhcp-10-31-74-2.dyn.mit.edu>
2021-10-21 19:53:58 -0400

    Change to functional components

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -7,55 +7,43 @@ import "./Skeleton.css";
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
 const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
 
-class Skeleton extends Component {
-  constructor(props) {
-    super(props);
-    // Initialize Default State
-    this.state = {};
-  }
-
-  componentDidMount() {
-    // remember -- api calls go here!
-  }
-
-  render() {
-    return (
-      <>
-        {this.props.userId ? (
-          <GoogleLogout
-            clientId={GOOGLE_CLIENT_ID}
-            buttonText="Logout"
-            onLogoutSuccess={this.props.handleLogout}
-            onFailure={(err) => console.log(err)}
-          />
-        ) : (
-          <GoogleLogin
-            clientId={GOOGLE_CLIENT_ID}
-            buttonText="Login"
-            onSuccess={this.props.handleLogin}
-            onFailure={(err) => console.log(err)}
-          />
-        )}
-        <h1>Good luck on your project :)</h1>
-        <h2> What we provide in this skeleton</h2>
-        <ul>
-          <li>Google Auth (Skeleton.js & auth.js)</li>
-          <li>Socket Infrastructure (client-socket.js & server-socket.js)</li>
-          <li>User Model (auth.js & user.js)</li>
-        </ul>
-        <h2> What you need to change</h2>
-        <ul>
-          <li>Change the font in utilities.css</li>
-          <li>Change the Frontend CLIENT_ID for Google Auth (Skeleton.js)</li>
-          <li>Change the Server CLIENT_ID for Google Auth (auth.js)</li>
-          <li>Change the Database SRV for Atlas (server.js)</li>
-          <li>Change the Database Name for MongoDB (server.js)</li>
-          <li>Add a favicon to your website at the path client/dist/favicon.ico</li>
-          <li>Update website title in client/dist/index.html</li>
-        </ul>
-      </>
-    );
-  }
-}
+const Skeleton = ({ userId, handleLogin, handleLogout }) => {
+  return (
+    <>
+      {userId ? (
+        <GoogleLogout
+          clientId={GOOGLE_CLIENT_ID}
+          buttonText="Logout"
+          onLogoutSuccess={handleLogout}
+          onFailure={(err) => console.log(err)}
+        />
+      ) : (
+        <GoogleLogin
+          clientId={GOOGLE_CLIENT_ID}
+          buttonText="Login"
+          onSuccess={handleLogin}
+          onFailure={(err) => console.log(err)}
+        />
+      )}
+      <h1>Good luck on your project :)</h1>
+      <h2> What we provide in this skeleton</h2>
+      <ul>
+        <li>Google Auth (Skeleton.js & auth.js)</li>
+        <li>Socket Infrastructure (client-socket.js & server-socket.js)</li>
+        <li>User Model (auth.js & user.js)</li>
+      </ul>
+      <h2> What you need to change</h2>
+      <ul>
+        <li>Change the font in utilities.css</li>
+        <li>Change the Frontend CLIENT_ID for Google Auth (Skeleton.js)</li>
+        <li>Change the Server CLIENT_ID for Google Auth (auth.js)</li>
+        <li>Change the Database SRV for Atlas (server.js)</li>
+        <li>Change the Database Name for MongoDB (server.js)</li>
+        <li>Add a favicon to your website at the path client/dist/favicon.ico</li>
+        <li>Update website title in client/dist/index.html</li>
+      </ul>
+    </>
+  );
+};
 
 export default Skeleton;
