efcdaac7dfc6befef28a292c8599a3b71bfa318e
Akshaj (Personal) <akshaj@dhcp-10-31-74-2.dyn.mit.edu>
2021-10-21 19:53:58 -0400

    Change to functional components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Router } from "@reach/router";
 import NotFound from "./pages/NotFound.js";
 import Skeleton from "./pages/Skeleton.js";
@@ -10,25 +10,19 @@ import { socket } from "../client-socket.js";
 import { get, post } from "../utilities";
 
 /**
- * Define the "App" component as a class.
+ * Define the "App" component
  */
-class App extends Component {
-  // makes props available in this component
-  constructor(props) {
-    super(props);
-    this.state = {
-      userId: undefined,
-    };
-  }
+const App = () => {
+  const [userId, setUserId] = useState(undefined);
 
-  componentDidMount() {
+  useEffect(() => {
     get("/api/whoami").then((user) => {
       if (user._id) {
         // they are registed in the database, and currently logged in.
         this.setState({ userId: user._id });
       }
     });
-  }
+  }, []);
 
   handleLogin = (res) => {
     console.log(`Logged in as ${res.profileObj.name}`);
@@ -44,21 +38,19 @@ class App extends Component {
     post("/api/logout");
   };
 
-  render() {
-    return (
-      <>
-        <Router>
-          <Skeleton
-            path="/"
-            handleLogin={this.handleLogin}
-            handleLogout={this.handleLogout}
-            userId={this.state.userId}
-          />
-          <NotFound default />
-        </Router>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Router>
+        <Skeleton
+          path="/"
+          handleLogin={this.handleLogin}
+          handleLogout={this.handleLogout}
+          userId={this.state.userId}
+        />
+        <NotFound default />
+      </Router>
+    </>
+  );
+};
 
 export default App;
