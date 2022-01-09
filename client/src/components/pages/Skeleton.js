a1e7a931877e63f4d59fc199a68bb88c70653842
Akshaj Kadaveru <akshajk@mit.edu>
2022-01-09 17:26:24 -0500

    Skeleton.js changes

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -26,22 +26,21 @@ const Skeleton = ({ userId, handleLogin, handleLogout }) => {
         />
       )}
       <h1>Good luck on your project :)</h1>
-      <h2> What we provide in this skeleton</h2>
+      <h2> What you need to change in this skeleton</h2>
       <ul>
-        <li>Google Auth (Skeleton.js & auth.js)</li>
-        <li>Socket Infrastructure (client-socket.js & server-socket.js)</li>
-        <li>User Model (auth.js & user.js)</li>
+        <li>
+          Change the Frontend CLIENT_ID (Skeleton.js) to your team's CLIENT_ID (obtain this at
+          http://weblab.to/clientid)
+        </li>
+        <li>Change the Server CLIENT_ID to the same CLIENT_ID (auth.js)</li>
+        <li>
+          Change the Database SRV (mongoConnectionURL) for Atlas (server.js). You got this in the
+          MongoDB setup.
+        </li>
+        <li>Change the Database Name for MongoDB to whatever you put in the SRV (server.js)</li>
       </ul>
-      <h2> What you need to change</h2>
-      <ul>
-        <li>Change the font in utilities.css</li>
-        <li>Change the Frontend CLIENT_ID for Google Auth (Skeleton.js)</li>
-        <li>Change the Server CLIENT_ID for Google Auth (auth.js)</li>
-        <li>Change the Database SRV for Atlas (server.js)</li>
-        <li>Change the Database Name for MongoDB (server.js)</li>
-        <li>Add a favicon to your website at the path client/dist/favicon.ico</li>
-        <li>Update website title in client/dist/index.html</li>
-      </ul>
+      <h2>How to go from this skeleton to our actual app</h2>
+      <a href="http://weblab.to/get-started">Check out this getting started guide</a>
     </>
   );
 };
