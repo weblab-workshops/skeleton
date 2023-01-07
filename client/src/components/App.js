a9198d38c3310ecb251e9bd9dab77598c599f8ee
Qiong Zhou Huang <qiongzhouh@gmail.com>
2023-01-07 18:35:54 -0500

    Add title

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,49 +1,86 @@
 import React, { useState, useEffect } from "react";
-import { Router } from "@reach/router";
-import NotFound from "./pages/NotFound.js";
-import Skeleton from "./pages/Skeleton.js";
 
-import "../utilities.css";
+const steakLookup = [
+  {
+    name: "Raw",
+    imgSrc:
+      "https://www.thedailymeal.com/img/gallery/can-you-go-to-a-steakhouse-and-order-a-completely-raw-steak/raw%20steak.JPG",
+  },
+  {
+    name: "Rare",
+    imgSrc:
+      "https://www.thespruceeats.com/thmb/D270QwEO0s-5PhP8xS_qW-bbyBc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/steak-doneness-from-rare-to-well-336362-03-8f1d80cb11c6489ebb0891ef9e7fa8aa.jpg",
+  },
+  {
+    name: "Medium",
+    imgSrc:
+      "https://www.theglobeandmail.com/resizer/01culVIZ2tzwXEloWLuaqk7Vsu0=/1200x798/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/tgam/Y5L47TVQNFHBXKINM2CKHUJWJE",
+  },
+  {
+    name: "Well Done!",
+    imgSrc:
+      "https://www.thespruceeats.com/thmb/e6ox29Qir-0dav0htiFrnBKARu0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/steak-doneness-from-rare-to-well-336362-06-e5cbe1ae15cc4f2a8f084684e4e0e9ac.jpg",
+  },
+  {
+    name: "Congratulations!",
+    imgSrc: "https://upload.wikimedia.org/wikipedia/commons/5/51/Charcoal_8.jpg",
+  },
+];
 
-import { socket } from "../client-socket.js";
+const Steak = () => {
+  const [doneness, setDoneness] = useState(0);
 
-import { get, post } from "../utilities";
+  const { name, imgSrc } = steakLookup[doneness];
+  const sentence = `Here is your ${name} steak!`;
 
-/**
- * Define the "App" component
- */
-const App = () => {
-  const [userId, setUserId] = useState(undefined);
-
-  useEffect(() => {
-    get("/api/whoami").then((user) => {
-      if (user._id) {
-        // they are registed in the database, and currently logged in.
-        setUserId(user._id);
-      }
-    });
-  }, []);
-
-  const handleLogin = (res) => {
-    console.log(`Logged in as ${res.profileObj.name}`);
-    const userToken = res.tokenObj.id_token;
-    post("/api/login", { token: userToken }).then((user) => {
-      setUserId(user._id);
-      post("/api/initsocket", { socketid: socket.id });
-    });
-  };
-
-  const handleLogout = () => {
-    setUserId(undefined);
-    post("/api/logout");
+  const sendToKitchen = () => {
+    setDoneness(doneness + 1);
   };
 
   return (
+    <div>
+      <h2>{sentence}</h2>
+      <img src={imgSrc} style={{ maxWidth: "500px" }} />
+      {doneness < 4 && (
+        <button style={{ fontSize: "24px" }} onClick={sendToKitchen}>
+          Send Back To Kitchen
+        </button>
+      )}
+    </div>
+  );
+};
+
+const Restaurant = () => {
+  const [showSteak, setShowSteak] = useState(false);
+
+  return (
+    <div
+      style={{
+        padding: "8px",
+        paddingTop: "0",
+        margin: "auto",
+        borderColor: "brown",
+        borderWidth: "4px",
+        borderStyle: "solid",
+        alignItems: "center",
+        width: "600px",
+      }}
+    >
+      <h1>Welcome to the web.lab Restaurant</h1>
+      <button style={{ fontSize: "24px" }} onClick={() => setShowSteak((showSteak) => !showSteak)}>
+        {showSteak ? "Delete Steak" : "Order Steak"}
+      </button>
+      {showSteak && <Steak />}
+    </div>
+  );
+};
+
+const App = () => {
+  return (
     <>
-      <Router>
-        <Skeleton path="/" handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} />
-        <NotFound default />
-      </Router>
+      <div style={{ margin: "16px", fontFamily: "Avenir" }}>
+        <Restaurant />
+      </div>
     </>
   );
 };
--- server/server.js
+++ server/server.js
@@ -31,68 +31,68 @@ const auth = require("./auth");
 // socket stuff
 const socketManager = require("./server-socket");
 
-// Server configuration below
-// TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
-// TODO change database name to the name you chose
-const databaseName = "FILL ME IN";
+// // Server configuration below
+// // TODO change connection URL after setting up your team database
+// const mongoConnectionURL = "FILL ME IN";
+// // TODO change database name to the name you chose
+// const databaseName = "FILL ME IN";
 
-// connect to mongodb
-mongoose
-  .connect(mongoConnectionURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: databaseName,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(`Error connecting to MongoDB: ${err}`));
+// // connect to mongodb
+// mongoose
+//   .connect(mongoConnectionURL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     dbName: databaseName,
+//   })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.log(`Error connecting to MongoDB: ${err}`));
 
-// create a new express server
+// // create a new express server
 const app = express();
 app.use(validator.checkRoutes);
 
-// allow us to process POST requests
-app.use(express.json());
+// // allow us to process POST requests
+// app.use(express.json());
 
-// set up a session, which will persist login data across requests
-app.use(
-  session({
-    secret: "session-secret",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+// // set up a session, which will persist login data across requests
+// app.use(
+//   session({
+//     secret: "session-secret",
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// );
 
-// this checks if the user is logged in, and populates "req.user"
-app.use(auth.populateCurrentUser);
+// // this checks if the user is logged in, and populates "req.user"
+// app.use(auth.populateCurrentUser);
 
-// connect user-defined routes
-app.use("/api", api);
+// // connect user-defined routes
+// app.use("/api", api);
 
-// load the compiled react files, which will serve /index.html and /bundle.js
-const reactPath = path.resolve(__dirname, "..", "client", "dist");
-app.use(express.static(reactPath));
+// // load the compiled react files, which will serve /index.html and /bundle.js
+// const reactPath = path.resolve(__dirname, "..", "client", "dist");
+// app.use(express.static(reactPath));
 
-// for all other routes, render index.html and let react router handle it
-app.get("*", (req, res) => {
-  res.sendFile(path.join(reactPath, "index.html"));
-});
+// // for all other routes, render index.html and let react router handle it
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(reactPath, "index.html"));
+// });
 
-// any server errors cause this function to run
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  if (status === 500) {
-    // 500 means Internal Server Error
-    console.log("The server errored when processing a request!");
-    console.log(err);
-  }
+// // any server errors cause this function to run
+// app.use((err, req, res, next) => {
+//   const status = err.status || 500;
+//   if (status === 500) {
+//     // 500 means Internal Server Error
+//     console.log("The server errored when processing a request!");
+//     console.log(err);
+//   }
 
-  res.status(status);
-  res.send({
-    status: status,
-    message: err.message,
-  });
-});
+//   res.status(status);
+//   res.send({
+//     status: status,
+//     message: err.message,
+//   });
+// });
 
 // hardcode port to 3000 for now
 const port = 3000;
