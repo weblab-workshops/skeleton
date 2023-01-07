db917f9f73ea3ce9de9d4a42a6147ac498fb2f5d
Qiong Zhou Huang <qiongzhouh@gmail.com>
2023-01-07 18:40:29 -0500

    Add back serving the bundle

--- server/server.js
+++ server/server.js
@@ -69,14 +69,14 @@ app.use(validator.checkRoutes);
 // // connect user-defined routes
 // app.use("/api", api);
 
-// // load the compiled react files, which will serve /index.html and /bundle.js
-// const reactPath = path.resolve(__dirname, "..", "client", "dist");
-// app.use(express.static(reactPath));
+// load the compiled react files, which will serve /index.html and /bundle.js
+const reactPath = path.resolve(__dirname, "..", "client", "dist");
+app.use(express.static(reactPath));
 
-// // for all other routes, render index.html and let react router handle it
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(reactPath, "index.html"));
-// });
+// for all other routes, render index.html and let react router handle it
+app.get("*", (req, res) => {
+  res.sendFile(path.join(reactPath, "index.html"));
+});
 
 // // any server errors cause this function to run
 // app.use((err, req, res, next) => {
