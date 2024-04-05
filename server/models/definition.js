5b2a682284b0eda31bb3a991f8f3f3d048431762
abayojosephdesire <67261480+abayojosephdesire@users.noreply.github.com>
2024-04-05 15:58:41 -0400

    Update definition.js

--- server/models/definition.js
+++ server/models/definition.js
@@ -1,62 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import { get } from "../../utilities";
-import CardDefinition from "../modules/CardDefinition.js";
+const mongoose = require("mongoose");
 
-const Definitions = (props) => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [definition, setDefinition] = useState(null);
-
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const q = searchParams.get("q");
-
-    if (!q) {
-      navigate("/");
-    } else {
-      get(`/api/definitions?q=${q}`).then((data) => {
-        if (data) {
-          setDefinition(data);
-        } else {
-          navigate("/NotFound");
-        }
-      }).catch((error) => {
-        navigate("/NotFound");
-      });
+const DefinitionSchema = new mongoose.Schema({
+  "Lemma.LemmaSign": String,
+  Sense: {
+    Example: {
+      'Example.Example': String,
+      'Example.Translation': String,
+      'Example.Source': String
+    },
+    TE: {
+      "TE.TE": String
     }
-  }, [navigate, location.search]);
+  },
+  "Lemma.Etymology": String,
 
-  return (
-    <div className="Definitions-container">
-      {definition ? (
-        Array.isArray(definition.Sense) ? (
-          definition.Sense.map((senseItem, index) => (
-            <CardDefinition
-              key={index}
-              wordEnglish={senseItem.TE?.["TE.TE"] || "No English word available"}
-              wordWopanaak={definition["Lemma.LemmaSign"] || "No Wopanaak word available"}
-              example={senseItem.Example?.['Example.Example'] || "No example available"}
-              translation={senseItem.Example?.['Example.Translation'] || "No translation available"}
-              source={senseItem.Example?.['Example.Source'] || "No source available"}
-              etymology={definition["Lemma.Etymology"] || "No etymology available"}
-            />
-          ))
-        ) : (
-          <CardDefinition
-            wordEnglish={definition.Sense.TE?.["TE.TE"] || "No English word available"}
-            wordWopanaak={definition["Lemma.LemmaSign"] || "No Wopanaak word available"}
-            example={definition.Sense.Example?.['Example.Example'] || "No example available"}
-            translation={definition.Sense.Example?.['Example.Translation'] || "No translation available"}
-            source={definition.Sense.Example?.['Example.Source'] || "No source available"}
-            etymology={definition["Lemma.Etymology"] || "No etymology available"}
-          />
-        )
-      ) : (
-        <div>Loading...</div>
-      )}
-    </div>
-  );
-};
+});
 
-export default Definitions;
+module.exports = mongoose.model("definition", DefinitionSchema);
